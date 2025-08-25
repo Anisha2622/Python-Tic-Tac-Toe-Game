17dad54a06032659328bb949310617cd671bdcf3
Anisha2622 <anishapawar0926@gmail.com>
2025-08-25 18:37:02 +0530

    Python-Tic-Tac-Toe-Game

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def prinyboard():
+    print(f"0 | 1 | 2")
+    print(f"--|---|---")
+    print(f"3 | 4 | 5")
+    print(f"--|---|---")
+    print(f"6 | 7 | 8")
     pass
     
 if __name__ == "__main__":
-    xState =[0, 0, 0, 0, 0, 0, 0, 0,]
-    zState =[0, 0, 0, 0, 0, 0, 0, 0,]
+    xState =[0, 0, 0, 0, 0, 0, 0, 0]
+    zState =[0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1 for X and 0 for O
 
     print("Welcome to to Tic Tac Toe")
+    print("X's chance")
+    prinyboard()
