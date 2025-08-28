8564ab8f057f3e67ee924e971fdd15c0b864ea44
Anisha2622 <anishapawar0926@gmail.com>
2025-08-28 11:17:04 +0530

    Tic Tac Toe

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-def printboard(xState, zState):
+import tkinter # tkinter is the standard GUI library for Python
 
-    print(f"{xState[0]?'X':(zState[0]?'0')} | 1 | 2")
-    print(f"--|---|---")
-    print(f"3 | 4 | 5")
-    print(f"--|---|---")
-    print(f"6 | 7 | 8")
-    pass
-    
-if __name__ == "__main__":
-    xState =[0, 0, 0, 0, 0, 0, 0, 0]
-    zState =[0, 0, 0, 0, 0, 0, 0, 0]
-    turn = 1 # 1 for X and 0 for O
+#game setup
+player = "X"
+player0 = "O"
+curr_player = player
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
 
-    print("Welcome to to Tic Tac Toe")
-    while(True):
-        printboard(xState, zState)
-        if(turn == 1):
-            print("X's chance")
-            value = int(input("please enter a value"))
-            xState[value] = 1
-        else:
-            print("X's chance")
-            value = int(input("please enter a value"))
-            zState[value] = 1
-        break
+color_blue = "#014D66"
+color_yellow = "#DADA05"
+color_grey = "#808080"
+color_light_gray = "#D3D3D3"
+
+#window setup
+window = tkinter.Tk()# create the game window
+window.title("Tic Tac Toe")
+window.resizable(False, False)
+
+frame = tkinter.Frame(window)
+label = tkinter.Label(frame, text=curr_player+"s turn", font=("consolas", 20), background=color_light_gray,
+                      foreground="white")
+
+label.pack()
+frame.pack()
+
+window.mainloop()# start the event loop
