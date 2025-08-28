3ba4af2fce7fab202b3df788a0ceab824486d251
Anisha2622 <anishapawar0926@gmail.com>
2025-08-28 11:29:15 +0530

    Tic Tac Toe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter # tkinter is the standard GUI library for Python
 
+def set_title(row, column):
+    pass
 #game setup
 player = "X"
 player0 = "O"
@@ -10,7 +12,7 @@ board = [[0, 0, 0],
 
 color_blue = "#014D66"
 color_yellow = "#DADA05"
-color_grey = "#808080"
+color_gray = "#808080"
 color_light_gray = "#D3D3D3"
 
 #window setup
@@ -22,7 +24,16 @@ frame = tkinter.Frame(window)
 label = tkinter.Label(frame, text=curr_player+"s turn", font=("consolas", 20), background=color_light_gray,
                       foreground="white")
 
-label.pack()
+label.grid(row=0, column=0)
+
+for row in range(3):
+    for column in range(3):
+        board[row][column] = tkinter.Button(frame, text="", font=("consolas", 50, "bold"), 
+                                        background=color_gray, foreground=color_blue, width=4, height=1,
+                                        command=lambda row=row, column= column: set_title(row, column))
+        board[row][column].grid(row=row+1, column=column)
+       
+
 frame.pack()
 
 window.mainloop()# start the event loop
